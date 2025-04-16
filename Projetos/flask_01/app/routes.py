322945536dd7e3fefe04883f9d1ca24d69519d65
Andrew Yamagata <andrewyamagata@gmail.com>
2025-04-16 11:45:56 -0300

    flask

--- Projetos/flask_01/app/routes.py
+++ Projetos/flask_01/app/routes.py
@@ -1,13 +1,6 @@
 from app import app
-from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    nome = 'Lopes'
-    dados = {"profissao": "Professora", "canal": "Profa. Alba Lopes"}
-    return render_template('index.html', nome=nome, dados=dados)
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+    return 'Hello, World'
