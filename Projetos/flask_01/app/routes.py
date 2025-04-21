91799ea4babcd419ea952bf187cba5c155ca1ae4
Andrew Yamagata <andrewyamagata@gmail.com>
2025-04-21 14:17:57 -0300

    user

--- Projetos/flask_01/app/routes.py
+++ Projetos/flask_01/app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template
 
+lista_usuarios = ['Lira', 'Amanda', 'Alon', 'Alessandra']
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -9,3 +11,7 @@ def index():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
