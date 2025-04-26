38b2cb530a9ec8d7d4a31117d90f86c966a022f7
Andrew Yamagata <andrewyamagata@gmail.com>
2025-04-25 22:51:08 -0300

    csrf token

--- Projetos/flask_01/app/routes.py
+++ Projetos/flask_01/app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 lista_usuarios = ['Lira', 'Amanda', 'Alon', 'Alessandra']
@@ -21,4 +21,13 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('index'))
+
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('index'))
+
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
