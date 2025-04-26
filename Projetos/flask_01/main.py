c144d8f97a1c7015cfa0a01ac201c5fb0ecd71db
Andrew Yamagata <andrewyamagata@gmail.com>
2025-04-26 09:37:58 -0300

    create a database

--- Projetos/flask_01/main.py
+++ Projetos/flask_01/main.py
@@ -1,3 +1,7 @@
 from app import app
+from flask_sqlalchemy import SQLAlchemy
 
 app.config['SECRET_KEY'] = 'senhadotoken'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+database = SQLAlchemy(app)
