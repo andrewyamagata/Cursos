8abe15a3a9c429f289134a351754d05d8707d33e
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2025-12-04 17:10:28 -0300

    Update script.js

--- JavaScript/01-Introdução/novaPasta/script.js
+++ JavaScript/01-Introdução/novaPasta/script.js
@@ -1,1 +1,33 @@
-console.log("Estamos dentro de uma outra pasta")
+console.log("Estamos dentro de uma outra pasta");
+// declaração do tipo + nome da variável = informação
+
+// TIPO LET
+let mensagem = "Olá Impressionador";
+console.log(mensagem);
+
+// declaração do tipo LET
+let cor = "vermelho";
+console.log(cor);
+// redeclaração não é permitido -> let cor = "azul"
+cor = "azul"; //reatribuição correta
+console.log(cor);
+
+// TIPO CONST
+const segundaMensagem = "Bem vindo ao curso Impressionador!";
+console.log(segundaMensagem);
+
+//tipo const não pode ser redeclarada ou criada novamente
+//não é mutável - não podemos reatribuir o valor -->> segundaMensagem = "Quero trocar a mensagem da minha variável";
+
+// TIPO VAR
+nome = "Nome"; //declaração está incorreto
+console.log(nome);
+
+var nome = "MeuNome";
+console.log(nome);
+
+var nome = "OutroNome"; //redeclararmos pode gerar erro
+console.log(nome);
+
+nome = "NossoNome";
+console.log(nome);
