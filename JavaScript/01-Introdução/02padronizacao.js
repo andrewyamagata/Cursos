56794ffb6746f2de1c084465c339b9d28160651e
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-19 16:15:56 -0300

    Update 02padronizacao.js

--- JavaScript/01-Introdução/02padronizacao.js
+++ JavaScript/01-Introdução/02padronizacao.js
@@ -2,5 +2,15 @@ let nomeProduto = "Boné";
 let precoProduto = 50;
 let quantidade = 3;
 
-let minhaPrimeiraMensagem = "Olá, bem vindo!";
+let minhaPrimeiraMensagem = "Olá, bem vindo!"; // Camel Case - Camelo
 
+// let 1novo = "Novo" - não pode começar com número, somente com '_' ou '$'
+
+let funcionario_email = "email@email.com"; // Snake Case
+
+let texto1 = "Dar ênfase na palavra: 'Essa' ";
+let texto2 = 'Dar ênfase na palavra: "Essa" '; // Menos utilizada
+
+//Para constante utilizar o exemplo
+const PI = 3.14;
+const MAX_WIDTH = 769;
