c9063618c9304c281d58908d87db3a3dcd7977de
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-19 17:21:59 -0300

    aula javascript

---
+++ JavaScript/01-Introdução/03tipodados.js
@@ -0,0 +1,12 @@
+// Tipo primitivos de dados
+// String - sequência de caracteres
+// "Um texto" - string
+// 'Criar uma mensagem' - string
+// `Criar mais um texto` - string template
+
+let mensagem = "Olá, Mundo!";
+let nome = "Paulo";
+let mensagem2 = `Olá, `;
+console.log(mensagem);
+console.log(nome);
+console.log(mensagem2);
