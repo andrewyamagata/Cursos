8b7eb65e616ecb2468faca84b59c6cc4bb004db6
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-20 14:55:25 -0300

    aula e exercicios javascript

--- JavaScript/01-Introdução/03tipodados.js
+++ JavaScript/01-Introdução/03tipodados.js
@@ -4,9 +4,62 @@
 // 'Criar uma mensagem' - string
 // `Criar mais um texto` - string template
 
-let mensagem = "Olá, Mundo!";
+let mensagem = "Olá, ";
 let nome = "Paulo";
-let mensagem2 = `Olá, `;
+let mensagem2 = `Olá, ${nome}. Bem vindo ao curso.`;
 console.log(mensagem);
 console.log(nome);
+
+console.log(mensagem + nome + ". Bem vindo!");
+
 console.log(mensagem2);
+
+// Índice = 0
+console.log(nome[0]);
+console.log((nome[0] = "S"));
+console.log(nome[0]);
+
+// Number
+let numero = 30;
+let numero1 = -56;
+let decimal = 6.78;
+// Infinity e NaN
+
+console.log(numero, numero1, decimal);
+
+let string = "10";
+let dez = 10;
+console.log(string, dez);
+console.log(string + dez);
+console.log(`${string} e ${dez}`);
+
+//Sistema Binário e Booleanos
+let ligado = true; // (verdadeiro = sim = 1)
+let desligado = false; // (falso = não = 0)
+
+console.log(true == 1);
+console.log(false == 0);
+
+let palavra = "Testando";
+console.log(Boolean(palavra));
+
+let numero0 = 0;
+console.log(Boolean(numero0));
+
+// Undefined e Null
+let mensagem3 = "Olá impressionador";
+let preco = 50.0;
+let logado = true;
+
+// Undefined
+let produto;
+let carrinho = undefined;
+console.log(produto);
+console.log(carrinho);
+console.log(typeof produto);
+console.log(typeof carrinho);
+
+// Null
+let lista = null;
+console.log(lista);
+console.log(typeof lista);
---
+++ JavaScript/01-Introdução/04exercicios.js
@@ -0,0 +1,42 @@
+//Exercícios
+//01.Escreva um programa simples que exiba uma string no terminal utilizando o console.log
+console.log("Olá, Mundo!");
+
+//02.Modifique o programa anterior para armazenar a mensagem em uma variável antes de exibi-la no consolelet string = "Olá, Mundo!";
+let string = "Olá, Mundo!";
+console.log(string);
+console.log(typeof string);
+
+//03.Declare três variáveis mutáveis: nome(string), idade(number) e isStudent(boolean).
+let nome = "Abobrinha";
+let idade = 20;
+let isStudent = true;
+
+//04.Modifique o valor das variáveis anteriores e imprima os novos valores no console.
+nome = "Prof Abobrinha";
+idade = 25;
+isStudent = false;
+
+console.log(nome);
+console.log(idade);
+console.log(isStudent);
+
+//05.Crie uma variável constante que irá armazenar um número e imprima no console.
+const numeric = 7.16;
+console.log(numeric);
+
+//06.Declare duas variáveis, endereco e telefone, sem atribuir valores a elas.
+let endereco;
+let telefone;
+
+//07.Declare variáveis com nomes descritivos para armazenar o nome de um produto, seu preço e a quantidade em estoque.
+let produto = "Feijão";
+let preco = 8.59;
+let qtd = 50;
+
+//08.Declare uma variável para armazenar o nome de uma cidade e outra para armazenar a sua população. Exiba uma mensagem combinando ambas as variáveis.
+let cidade = "Mogi das Cruzes";
+let populacao = 450000;
+console.log(
+    `A cidade de ${cidade} possui uma população de ${populacao} habitantes`,
+);
