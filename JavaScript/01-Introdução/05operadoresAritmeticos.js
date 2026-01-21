5fa9b313bcc3787ec4046789bd046c12e2ea4dac
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-21 16:15:06 -0300

    Update 05operadoresAritmeticos.js

---
+++ JavaScript/01-Introdução/05operadoresAritmeticos.js
@@ -0,0 +1,30 @@
+//Operações Básicas em Javascript
+//Adição
+let valor = 10;
+let segundoValor = -43;
+
+console.log(valor + segundoValor);
+
+let mensagem = "20";
+console.log(mensagem + valor); //concatenação(string + number)
+
+//Subtração
+console.log(valor - segundoValor);
+console.log(segundoValor - valor);
+
+//Divisão
+console.log(valor / segundoValor);
+console.log(segundoValor / valor);
+
+//Módulo - resto da divisão
+console.log(valor % segundoValor);
+console.log(segundoValor % valor);
+
+//Operações Avançadas em Javascript
+//Exponenciação
+let base = 2;
+let expoente = 3;
+let resultado = base ** expoente;
+console.log(resultado);
+
+// Incremento ++ (adicionar o valor de 1) Decremento -- (diminuir o valor 1)
