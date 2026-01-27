d550975b023bd4d17daec1e2520197f40f02e743
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-27 17:00:35 -0300

    Update 02chamadafuncao.js

---
+++ JavaScript/02-Funções/02chamadafuncao.js
@@ -0,0 +1,9 @@
+// Chamadas de funções
+
+function exibirDetalhes(nome, curso) {
+    console.log("Professor: " + nome + " Curso: " + curso); // instrução
+}
+
+exibirDetalhes("Eleuterio", "Química");
+
+// Parâmetros e Argumentos
