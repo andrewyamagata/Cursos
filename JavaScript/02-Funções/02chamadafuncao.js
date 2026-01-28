d87ec6211a92be535a693e36118f0e892f4c0e29
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-01-28 14:31:32 -0300

    Update 02chamadafuncao.js

--- JavaScript/02-Funções/02chamadafuncao.js
+++ JavaScript/02-Funções/02chamadafuncao.js
@@ -7,3 +7,27 @@ function exibirDetalhes(nome, curso) {
 exibirDetalhes("Eleuterio", "Química");
 
 // Parâmetros e Argumentos
+
+// function nomeFuncao(PARÂMETRO) {INSTRUÇÃO}
+
+function soma(numero1, numero2) {
+    console.log(numero1 + numero2);
+}
+
+// nomeFunction(ARGUMENTO) - Chamada da função (executa o bloco de instruções)
+soma(8, 57);
+
+// Exemplo
+function calcularPrecoTotal(precoUnitario, quantidade) {
+    let total = precoUnitario * quantidade;
+    console.log("O total da sua compra é: R$ " + total);
+}
+calcularPrecoTotal(37.9, 128);
+// Pode ser feito assim também
+function calcularPrecoTotal(precoUnitario, quantidade) {
+    let total = precoUnitario * quantidade;
+    console.log("O total da sua compra é: R$ " + total);
+}
+let camiseta = 29.58;
+let quantidadeItem = 39;
+calcularPrecoTotal(camiseta, quantidadeItem);
