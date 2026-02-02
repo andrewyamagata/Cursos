11f0330f48f7df0a64304c61e4d46470fd86e624
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-02-02 09:32:52 -0300

    escopo javascript

--- JavaScript/02-Funções/07escopo.js
+++ JavaScript/02-Funções/07escopo.js
@@ -1,3 +1,18 @@
 // Escopo Global - Local (Função) - Bloco
 
 let global = "Sou do escopo Global";
+
+function mensagem() {
+    // escopo local da minha função
+    let local = "Sou local da função";
+    console.log(local);
+}
+
+mensagem();
+console.log(global);
+
+// não é uma função - escopo de bloco
+if (true) {
+    let bloco = "Sou do bloco do IF";
+    console.log(bloco);
+}
