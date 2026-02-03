f271a5f6d16336119b1bbebe94ad2ac07dc177ca
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-02-03 16:44:06 -0300

    metodos javascript

---
+++ JavaScript/03-Métodos/01introducao.js
@@ -0,0 +1,14 @@
+// Métodos - funções associadas a objetos que permitem realizar ações ou manipular os dados desses objetos.
+// MÉTODOS NATIVOS
+
+//Converte uma string para maiúsculas
+toUpperCase();
+
+//Verifica se uma string contém outra string
+includes();
+
+//Adicionar um elemento ao final de um array
+push();
+
+//Verifica se um objeto possui uma propriedade específica
+hasOwnProperty();
