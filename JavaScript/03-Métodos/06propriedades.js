8deb699bbbab0045cccba27e78fb9a9b29823f9f
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-02-11 17:12:39 -0300

    propriedades javascript

---
+++ JavaScript/03-Métodos/06propriedades.js
@@ -0,0 +1,14 @@
+// Propriedades em Javascript
+
+const objeto = {
+    propriedade: 10,
+    metodos: function () {
+        console.log("Olá eu sou um método");
+    },
+};
+
+const string = "Eu sou louco por javascript";
+console.log(string.length);
+
+const array = [1, 30, 46, 4];
+console.log(array.length);
