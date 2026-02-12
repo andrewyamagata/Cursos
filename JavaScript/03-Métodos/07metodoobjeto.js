a118cdbdd3e78558676f5ea460961a7fe53578a2
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-02-12 17:16:17 -0300

    objetos javascript

---
+++ JavaScript/03-Métodos/07metodoobjeto.js
@@ -0,0 +1,9 @@
+// Métodos Nativos - Objeto
+const produto = {
+    nome: "Laptop",
+    preco: 2500,
+    disponibilidade: true,
+};
+
+console.log(Object.keys(produto));
+console.log(Object.values(produto));
