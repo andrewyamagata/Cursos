fedc796d21efcedb42ae7ab3e700e2fd32e19c77
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-02-19 12:39:36 -0300

    metodos javascript

--- JavaScript/03-Métodos/07metodoobjeto.js
+++ JavaScript/03-Métodos/07metodoobjeto.js
@@ -1,8 +1,8 @@
 // Métodos Nativos - Objeto
 const produto = {
-  nome: "Laptop",
-  preco: 2500,
-  disponibilidade: true,
+    nome: "Laptop",
+    preco: 2500,
+    disponibilidade: true,
 };
 
 console.log(Object.keys(produto)); // chave em uma lista
@@ -18,13 +18,13 @@ Object.defineProperty(produto, "disponibilidade", { value: false }); // modifica
 console.log(produto);
 
 const pessoa = {
-  nome: "Carlos",
-  idade: 30,
+    nome: "Carlos",
+    idade: 30,
 };
 
 const trabalho = {
-  profissao: "Engenheiro",
-  cidade: "São Paulo",
+    profissao: "Engenheiro",
+    cidade: "São Paulo",
 };
 
 const funcionario = {};
@@ -32,13 +32,38 @@ Object.assign(funcionario, pessoa, trabalho); // copia para um objeto destino
 console.log(funcionario);
 
 Object.defineProperty(funcionario, "salario", {
-  // adiciona uma nova propriedade, porém mais complicado
-  value: 2000,
-  enumerable: true,
-  writable: true,
-  configurable: true,
+    // adiciona uma nova propriedade, porém mais complicado
+    value: 2000,
+    enumerable: true,
+    writable: true,
+    configurable: true,
 });
 console.log(funcionario);
 
 delete funcionario.salario; // deletando propriedade
 console.log(funcionario);
+
+//
+
+const pessoas = {
+    cidade: "Rio de Janeiro",
+    surf: true,
+    falar: function () {
+        console.log("Olá!");
+    },
+};
+
+const carro = { modelo: "Corolla", marca: "Toyota" };
+
+const joao = Object.create(pessoas); // Herdade de um objeto
+Object.assign(joao, carro); // Copiar as propriedades
+joao.falar();
+console.log(joao.cidade, joao.surf);
+
+joao.nome = "João"; // propriedade nome
+joao.idade = 30; // propriedade idade
+console.log(joao);
+
+console.log(joao.hasOwnProperty("nome"));
+console.log(joao.hasOwnProperty("marca"));
+console.log(joao.hasOwnProperty("surf"));
