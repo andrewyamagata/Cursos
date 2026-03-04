d527deb988e37664c18be2dfbbb5e4b80780c47f
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-03-04 17:00:24 -0300

    metodos number javascript

--- JavaScript/03-Métodos/10metodonumber.js
+++ JavaScript/03-Métodos/10metodonumber.js
@@ -17,3 +17,25 @@ console.log(numeroPontoFlutuante.toPrecision(4)); // número dígitos que o nume
 console.log(inteiro.toPrecision(1)); // O número é arredondado e exibido em notação (4 vezes 10 elevado a 1)
 console.log(inteiro.toPrecision(2)); // 42
 console.log(inteiro.toPrecision(3)); // 42.0
+
+let flutuanteString = "32.7869";
+let inteiroString = "42";
+
+console.log(typeof flutuanteString);
+
+//Converter o valor (geralmente string) para um número
+console.log(typeof Number.parseFloat(flutuanteString));
+console.log(Number.parseFloat(inteiroString));
+
+//Converter um inteiro para uma string
+let numero = 43;
+console.log(numero.toString()); //base decimal, não precisa colocar 10
+console.log(numero.toString(2)); //base binária
+console.log(numero.toString(8)); //base octal
+//base vai de 2 - 36
+
+//Converter uma string para um número inteiro, considerando a base numérica (opcional)
+console.log(Number.parseInt(flutuanteString));
+console.log(Number.parseInt(inteiroString));
+
+console.log(Number.parseFloat(flutuanteString).toFixed(2));
