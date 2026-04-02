f5bfcbb90b883976026d0ce1c38dc6d5b23bccf9
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-04-02 16:15:45 -0300

    exercicios javascript

---
+++ JavaScript/03-Métodos/11exercicio.js
@@ -0,0 +1,20 @@
+//Exercício 1: Converta valores de texto em números
+//Imagine que você tem valores em formato de texto, como "150.50" ou "200px". Seu objetivo é
+//convertê-los para números reais.
+
+// Exercício 2: Somar valores numéricos de uma entrada de formulário
+//Você recebe dois valores numéricos como string. Converta-os e some-os corretamente.
+
+// Exercício 3: Arredonde valores financeiros
+//Você tem um valor financeiro com várias casas decimais e deseja exibi-lo com apenas duas
+//casas decimais.
+
+//Exercício 4: Ajustar a precisão de um número
+//Você tem um número muito grande e precisa exibi-lo com uma precisão específica.
+
+// Exercício 5: Verificar se a entrada é um número válido
+//  Você recebe uma entrada do usuário e precisa garantir que seja um número válido antes de
+//  realizar qualquer operação.
+
+//  Exercício 6: Verificar se o número é finito
+//  Você quer garantir que um número não seja Infinity ou NaN antes de usá-lo em cálculos.
