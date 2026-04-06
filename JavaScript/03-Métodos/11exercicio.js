59e6890159fe313c35a1dc148d9d04b2299f6284
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-04-06 17:08:05 -0300

    exercicio javascript

--- JavaScript/03-Métodos/11exercicio.js
+++ JavaScript/03-Métodos/11exercicio.js
@@ -2,8 +2,15 @@
 //Imagine que você tem valores em formato de texto, como "150.50" ou "200px". Seu objetivo é
 //convertê-los para números reais.
 
+const valor1 = "150.50";
+const valor2 = "200px";
+const valor3 = "abc";
+
 // Exercício 2: Somar valores numéricos de uma entrada de formulário
 //Você recebe dois valores numéricos como string. Converta-os e some-os corretamente.
+
+const numero1 = "10";
+const numero2 = "20.5";
 
 // Exercício 3: Arredonde valores financeiros
 //Você tem um valor financeiro com várias casas decimais e deseja exibi-lo com apenas duas
