526aa1420157291418ac526702dc4b3ab5f50a2a
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-04-07 17:13:58 -0300

    exercicios javascript

--- JavaScript/03-Métodos/11exercicio.js
+++ JavaScript/03-Métodos/11exercicio.js
@@ -6,11 +6,21 @@ const valor1 = "150.50";
 const valor2 = "200px";
 const valor3 = "abc";
 
+console.log(Number.parseFloat(valor1));
+console.log(Number.parseInt(valor2));
+console.log(Number.parseFloat(valor3));
+
 // Exercício 2: Somar valores numéricos de uma entrada de formulário
 //Você recebe dois valores numéricos como string. Converta-os e some-os corretamente.
 
 const numero1 = "10";
 const numero2 = "20.5";
+
+const soma = Number.parseFloat(numero1) + Number.parseFloat(numero2);
+console.log(soma);
+
+const soma2 = parseFloat(numero1) + parseFloat(numero2); //consegue interpretar que é um número ao usar parseFloat
+console.log(soma2);
 
 // Exercício 3: Arredonde valores financeiros
 //Você tem um valor financeiro com várias casas decimais e deseja exibi-lo com apenas duas
