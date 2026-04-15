9017b0ccdb7fbf8087d06412ce65350e258b2574
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-04-15 16:55:22 -0300

    objetos javascript

--- JavaScript/03-Métodos/12objetosglobais.js
+++ JavaScript/03-Métodos/12objetosglobais.js
@@ -1,8 +1,32 @@
 //Objetos Globais
 //Math = operações matemáticas e Date = datas
 
+// Operações:
+
 const PI = Math.PI;
 console.log(PI);
 
 const raizQuadrada = Math.sqrt(16);
 console.log(raizQuadrada);
+
+const potencia = Math.pow(2, 8);
+console.log(potencia);
+
+// Arredondamento
+
+let numero = 3.45; // 4 - para baixo
+let numero2 = 3.55; // 5 - para cima
+
+console.log(Math.round(numero));
+console.log(Math.round(numero2));
+
+//Sem usar o operador Math
+console.log(parseInt(numero));
+console.log(parseInt(numero2));
+
+// Valor aleatório entre 0 e 1
+
+console.log(Math.random());
+
+const aleatório = Math.random() * 100;
+console.log(parseFloat(aleatório.toFixed(2)));
