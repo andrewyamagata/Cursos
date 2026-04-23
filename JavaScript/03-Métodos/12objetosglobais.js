bdd3feb3d33725ed39f2bfbe6dc6bc7f743625bf
Andrew Yamagata <67279172+andrewyamagata@users.noreply.github.com>
2026-04-23 17:06:26 -0300

    objetos globais

--- JavaScript/03-Métodos/12objetosglobais.js
+++ JavaScript/03-Métodos/12objetosglobais.js
@@ -30,3 +30,21 @@ console.log(Math.random());
 
 const aleatório = Math.random() * 100;
 console.log(parseFloat(aleatório.toFixed(2)));
+
+const aleatorio2 = Number.parseFloat((Math.random() * 100).toFixed(2));
+console.log(aleatorio2);
+
+// Math.min() - retorna o valor mínimo encontrado dentro de uma lista
+// Math.max() - retorna o valor máximo encontrado dentro de uma lista
+
+console.log(Math.min(2, 45, 6, 87, 43));
+console.log(Math.min([2, 45, 6, 87, 43])); // erro - retorna NaN
+
+console.log(Math.max(2, 45, 6, 87, 43));
+
+// Forma para fazer com lista
+
+let lista = [2, 45, 6, 87, 43, 101];
+
+console.log(Math.min(...lista)); // spread operador .. (espalha informações da lista)
+console.log(Math.max(...lista));
